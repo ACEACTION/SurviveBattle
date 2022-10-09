16f8c9b0dc89f2a89351e430c5b46e4ee24e7f10
mohammad firoozi <72497068+mohammadfiroozi1@users.noreply.github.com>
2022-10-09 14:55:26 +0330

    scriptable obj recreate

--- suvival/Assets/Scripts/Enemies/EnemyStats.cs
+++ suvival/Assets/Scripts/Enemies/EnemyStats.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Pool;
 [CreateAssetMenu(menuName = "EnemyStats")]
 public class EnemyStats : ScriptableObject
 {
     public float hp;
     public float movespeed;
+    [SerializeField] GameObject lootEffectPrefab;
 
 
 
